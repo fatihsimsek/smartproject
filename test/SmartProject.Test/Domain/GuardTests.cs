b54a8df8e090102ee1d39f8ad58363d34f3df794
Fatih Şimşek <fatihsimsek@Fatih-Simsek.local>
2024-03-23 12:12:55 +0300

    add test

---
+++ test/SmartProject.Test/Domain/GuardTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SmartProject.Domain.Common;
+
+namespace SmartProject.Test.Domain
+{
+    [TestFixture]
+    public class GuardTests
+	{
+		[Test]
+		public void ValueIsNotEmpty()
+		{
+			string value = "SmartProject";
+			Assert.DoesNotThrow(() => Guard.AgainstEmptyString(value));
+		}
+	}
+}
+
