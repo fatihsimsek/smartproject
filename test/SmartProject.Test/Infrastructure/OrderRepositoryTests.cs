e165f7d13e7410031bff5fca4bc35fd94e384a9d
Fatih Şimşek <fatihsimsek@Fatih-Simsek.local>
2024-03-25 09:34:52 +0300

    add dbcontext test

---
+++ test/SmartProject.Test/Infrastructure/OrderRepositoryTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using SmartProject.Domain.Features;
+using SmartProject.Infrastructure;
+using SmartProject.Infrastructure.Features;
+
+namespace SmartProject.Test.Infrastructure
+{
+	public class OrderRepositoryTests
+	{
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetByIdNotEmpty()
+        {
+            Order returnValue = new Order() { Id = 1, CustomerId = 1, ProductId = 1 };
+
+            var moqSmartDbContext = new Mock<ISmartDbContext>();
+            var moqOrderDbSet = new Mock<DbSet<Order>>();
+
+            moqOrderDbSet.Setup(o => o.Find(It.IsAny<long>())).Returns(returnValue);
+            moqSmartDbContext.Setup(m => m.Set<Order>()).Returns(moqOrderDbSet.Object);
+            
+            var orderRepository = new OrderRepository(moqSmartDbContext.Object);
+            var testValue = orderRepository.GetById(1);
+
+            Assert.That(testValue!.Id, Is.EqualTo(returnValue.Id));
+        }
+    }
+}
+
